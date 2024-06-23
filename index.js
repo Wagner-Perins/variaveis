373b56d23f9a0b53ad78745ead2d0443500abe1e
Wagner Perins <wagnerperins@gmail.com>
2024-06-23 17:57:03 -0300

    Var, Let e Const

---
+++ index.js
@@ -0,0 +1,36 @@
+//Utilizando a variável descontinuada chamada VAR
+
+var nome = 'Sora';
+
+console.log(nome);
+
+nome = 'Ogamy';
+
+console.log(nome);
+
+//Utilizando a variável LET
+
+let nomeCompleto = 'Ogamy Sora';
+let idade = 31;
+
+console.log(nomeCompleto, idade);
+
+//Variável Let e Var podem ser renomeadas
+
+nomeCompleto = 'Sora Ogamy';
+idade = 30;
+
+console.log(nomeCompleto, idade);
+
+//Utilizando a variável CONST
+
+const email = 'sora.natsuki999@gmail.com';
+
+console.log(email);
+
+//Variável Const não pode ser renomeada
+
+//email = 'soralalau@gami.com';
+
+//console.log(email)
+
