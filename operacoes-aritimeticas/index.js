3a1cd98930d838455315c87f2d286f5b524f4742
Wagner Perins <wagnerperins@gmail.com>
2024-06-24 19:52:18 -0300

    Operação aritimética

---
+++ operacoes-aritimeticas/index.js
@@ -0,0 +1,12 @@
+let numero1 = 7;
+let numero2 = 10;
+
+let soma = numero1 + numero2;
+let subtracao = numero2 - numero1;
+let multiplicacao = numero1 * numero2;
+let divicao = numero1 / numero2;
+
+console.log(soma);
+//console.log(subtracao);
+//console.log(multiplicacao);
+//console.log(divicao);
